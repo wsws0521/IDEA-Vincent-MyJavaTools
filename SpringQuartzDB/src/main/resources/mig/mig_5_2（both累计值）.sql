ea97b5c88cc1d8bdecfaa4821752f63494e397df
wsws0521 <623540439@qq.com>
2020-06-10 20:04:34 +0200

    Centlec迁移，增加外链服务地址配置 累计值迁移，tv保存实际的lastvenddate字段值

--- SpringQuartzDB/src/main/resources/mig/mig_5_2（both累计值）.sql
+++ SpringQuartzDB/src/main/resources/mig/mig_5_2（both累计值）.sql
@@ -33,7 +33,8 @@ BEGIN
 		START_TIME,
 		END_TIME,
 		CUMU_VALUE,
-		VALUE_UNIT)
+		VALUE_UNIT,
+		tv)
 	SELECT
 		2,
 		AMI_GET_SEQUENCE('SEQ_VD_C_CUMU_VALUE'),
@@ -45,7 +46,8 @@ BEGIN
 		LAST_DAY(CURDATE()), -- 累计结束时间
 		-- DATE_ADD(DATE_ADD(CURDATE(), INTERVAL -DAY(CURDATE())+1 DAY), INTERVAL 1 MONTH),
 		a.energy,
-		'KWH'
+		'KWH',
+		a.LASTVENDDATE
 	FROM tmp_ljz a, A_EQUIP_METER b
     WHERE a.mt_comm_addr  = b.assetno and a.ISFREE = 0 and a.ISUSED = 0;
 
@@ -55,14 +57,15 @@ BEGIN
 
 	# 3-表计本月已享受免费购电，添加到绑定用户的累计值
 	INSERT INTO vd_c_cumu_value
-		(lessee_id, cumu_id, rule_id, cumu_obj, cumu_obj_id, cumu_item, start_time, end_time, cumu_value, value_unit)
+		(lessee_id, cumu_id, rule_id, cumu_obj, cumu_obj_id, cumu_item, start_time, end_time, cumu_value, value_unit, tv)
 	SELECT
 		2, AMI_GET_SEQUENCE('SEQ_VD_C_CUMU_VALUE'), NULL, '01', d.CONS_ID, '0101',
 		DATE_ADD(CURDATE(), INTERVAL -DAY(CURDATE()) + 1 DAY), -- 累计开始时间
 		LAST_DAY(CURDATE()), -- 累计结束时间
 		-- DATE_ADD(DATE_ADD(CURDATE(), INTERVAL -DAY(CURDATE())+1 DAY), INTERVAL 1 MONTH),
 		50, -- 免费额度使用50kwh，默认ljz里面的此类energy都是50
-		'KWH'
+		'KWH',
+		a.LASTVENDDATE
     from tmp_ljz a
     INNER JOIN A_EQUIP_METER b on a.mt_comm_addr  = b.assetno
     INNER JOIN a_mp_equipment_rela c on b.METER_ID = c.EQUIPMENTID and c.EQUIPMENTTYPE = '02'
