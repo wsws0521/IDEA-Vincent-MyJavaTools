62d57fc7ed530893eedeeadbf77baf2b83c01d4a
wsws0521 <623540439@qq.com>
2020-06-28 15:46:59 +0200

    尝试改造Springboot为war（方便放入Tomcat运行） 尝试添加web.xml，模拟xmlvend的支持SSL请求 验证自签证书过期为什么依旧能访问服务的问题

--- EasyCode/src/main/java/cn/vincent/easycode/EasyCodeApplication.java
+++ EasyCode/src/main/java/cn/vincent/easycode/EasyCodeApplication.java
@@ -3,10 +3,27 @@ package cn.vincent.easycode;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("cn.vincent.easycode.dao")
-public class EasyCodeApplication {
+public class EasyCodeApplication extends SpringBootServletInitializer {
+    /**
+     * Configure the application. Normally all you would need to do it add sources (e.g.
+     * config classes) because other settings have sensible defaults. You might choose
+     * (for instance) to add default command line arguments, or set an active Spring
+     * profile.
+     *
+     * @param builder a builder for the application context
+     * @return the application builder
+     * @see SpringApplicationBuilder
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EasyCodeApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EasyCodeApplication.class);
     }
