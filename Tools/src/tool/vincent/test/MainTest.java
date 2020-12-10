412cc7f06689859f01afeb7573f9a2ce933c347c
LAPTOP-33HLVBEA\Vincent <623540439@qq.com>
2020-12-10 16:49:53 +0800

    delete *.iml

--- Tools/src/tool/vincent/test/MainTest.java
+++ Tools/src/tool/vincent/test/MainTest.java
@@ -7,25 +7,31 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *
+ */
 public class MainTest {
     public static void main(String[] args) {
-        Map<String, Object> importResult = new HashMap<>();
-        importResult.put("errorCode", "00");
+//        Map<String, Object> importResult = new HashMap<>();
+//        importResult.put("errorCode", "00");
+////        importResult.put("sdcssx", "2000000000");
 //        importResult.put("sdcssx", "2000000000");
-        importResult.put("sdcssx", "2000000000");
-        importResult.put("expirationDate", "20210719220000");
-//        int num = (int) importResult.get("sdcssx");
-        int num = Integer.parseInt(importResult.get("sdcssx").toString());
+//        importResult.put("expirationDate", "20210719220000");
+////        int num = (int) importResult.get("sdcssx");
+//        int num = Integer.parseInt(importResult.get("sdcssx").toString());
+//
+//        Date date = null;
+//        try {
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+//            date = sdf.parse(importResult.get("expirationDate").toString());
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(num);
+//        System.out.println(date);
+        int a = 12;
 
-        Date date = null;
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-            date = sdf.parse(importResult.get("expirationDate").toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        System.out.println(num);
-        System.out.println(date);
+        System.out.println(a+=a-=a*a);
     }
 
 }
