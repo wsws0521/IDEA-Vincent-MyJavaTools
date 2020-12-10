38d64505db6d15e6328c7bbf71899bad9cffa7ab
LAPTOP-33HLVBEA\Vincent <623540439@qq.com>
2020-12-10 16:33:53 +0800

    无用的测试

---
+++ Tools/src/tool/vincent/test/Test001.java
@@ -0,0 +1,14 @@
+package tool.vincent.test;
+
+/**
+ * @Author Vincent
+ * @Description
+ * @Date 13:24 2020/12/3
+ **/
+public class Test001 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Tools/src/tool/vincent/test/Test20201203.java
@@ -0,0 +1,7 @@
+package tool.vincent.test;
+
+/**
+ *
+ */
+public class Test20201203 {
+}
