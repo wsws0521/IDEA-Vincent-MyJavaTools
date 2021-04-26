b8b193743a27d94fe66bfa158c8f058de49e9642
VincentLaptop <623540439@qq.com>
2021-04-26 16:31:52 +0800

    用建造者模式 优雅 多属性对象创建

---
+++ Tools/src/tool/vincent/testfinal/OuterClass.java
@@ -0,0 +1,23 @@
+package tool.vincent.testfinal;
+
+public class OuterClass {
+    public void useInnerClass(int num){
+        InnerClass ic = new InnerClass(){
+            @Override
+            void doSomething() {
+                super.doSomething();
+                System.out.println("num is : " + num);
+            }
+        };
+        ic.doSomething();
+    }
+
+    public class InnerClass{
+        void doSomething(){}
+    }
+
+    public static void main(String[] args) {
+        OuterClass oc = new OuterClass();
+        oc.useInnerClass(5);
+    }
+}
